f7d74782399c26a74e3c28b555eef26f199f7123
reza buzarjemehri <dev.reza21@gmail.com>
2024-08-16 16:13:37 +0330

    add resume

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -20,13 +20,15 @@ const About = ({ lang }: { lang: Language }) => {
 					{lang === "en"
 						? aboutme.en.map((line, index) => <p key={index}>{line}</p>)
 						: aboutme.fa.map((line, index) => <p key={index}>{line}</p>)}
-					{/* <Link
-						href="/"
+					<Link
+						href="/Resume.pdf"
+						target="_blank"
+						download={{}}
 						className="px-0.5 inline-block py-0.5 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-center text-white mt-12 sm:mt-24 font-semibold">
 						<span className="block bg-[#121212] hover:bg-[#090909] rounded-full px-5 py-1.5 sm:py-2">
 							{lang === "en" ? "Downlaod resume" : "دانلود رزومه"}
 						</span>
-					</Link> */}
+					</Link>
 				</div>
 			</div>
 		</section>
