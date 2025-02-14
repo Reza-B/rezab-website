6776513cf76883cbe48b902da9cf20e0689c4e96
Reza Buzarjemehri <dev.reza21@gmail.com>
2025-02-14 13:47:02 +0330

    add SpeedInsights component to layout for performance monitoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,26 @@
 import type { Metadata } from "next";
 import { Vazirmatn } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const vazirmatn = Vazirmatn({ subsets: ["arabic"] });
 
 export const metadata: Metadata = {
-	title: "Reza Buzarjemehri",
-	description: "full stack developer",
+  title: "Reza Buzarjemehri",
+  description: "full stack developer",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={vazirmatn.className}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={vazirmatn.className}>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
 }
